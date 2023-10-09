8a44a249f6ecc14c78b3bcd46ef10ac031457e9a
Gaetan <gaetan@cnty.ai>
2023-10-09 11:19:59 +0200

    do: Add tests

--- api/tests/Api/GreetingsTest.php
+++ api/tests/Api/GreetingsTest.php
@@ -3,25 +3,33 @@
 namespace App\Tests\Api;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Services\DevGreeter;
+use App\Services\FriendGreeter;
 
 class GreetingsTest extends ApiTestCase
 {
     public function testCreateGreeting(): void
     {
-        static::createClient()->request('POST', '/greetings', [
+        $client = static::createClient();
+        $container = static::getContainer();
+
+        $temp = $this->createMock(FriendGreeter::class);
+        $temp->expects($this->once())->method('greet')->willReturn('Friend');
+        $container->set(FriendGreeter::class, $temp);
+
+        $temp2 = $this->createMock(DevGreeter::class);
+        $temp2->expects($this->once())->method('greet')->willReturn('Friend');
+        $container->set(DevGreeter::class, $temp2);
+
+        $client->request('POST', '/greetings', [
             'json' => [
-                'name' => 'Kévin',
+                'name' => 'Gaetan',
             ],
             'headers' => [
                 'Content-Type' => 'application/ld+json',
             ],
         ]);
 
-        $this->assertResponseStatusCodeSame(201);
-        $this->assertJsonContains([
-            '@context' => '/contexts/Greeting',
-            '@type' => 'Greeting',
-            'name' => 'Kévin',
-        ]);
+        $this->assertResponseStatusCodeSame(202);
     }
 }
